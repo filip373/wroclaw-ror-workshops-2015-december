5c8e03be528dc7f0edb4def57601ed2e0a4adc46
Filip Soszyński <filip373@gmail.com>
2016-10-08 20:13:47 +0200

    Production and development seeds (#4)
    Added seeds depending on environment, production and development

---
+++ db/seeds/production.rb
@@ -0,0 +1,42 @@
+TEACHER_TITLES = %w(Dr. Prof. TA)
+NAMES = %w(John Peter Andrew Robert Mike Anthony)
+LAST_NAMES = %w(Smith Thompson Adams Anderson Carl Cooper)
+
+3.times do
+  Teacher.create!(
+    first_name: NAMES.sample,
+    last_name: LAST_NAMES.sample,
+    academic_title: TEACHER_TITLES.sample
+  )
+end
+
+teachers = Teacher.all
+['Daily routine', 'Sports', 'Means of transport',
+ 'Work experience', 'Countries'].each do |title|
+  SubjectItem.create!(
+    title: title,
+    teacher: teachers.sample
+  )
+end
+
+25.times do
+  Student.create!(
+    first_name: NAMES.sample,
+    last_name: LAST_NAMES.sample
+  )
+end
+
+students = Student.all
+SubjectItem.all.each do |subject_item|
+  subject_item.students << students.sample(rand(1..4))
+end
+
+SubjectItem.all.each do |subject_item|
+  subject_item.students.each do |student|
+    rand(1..5).times do
+      subject_item.subject_item_notes << SubjectItemNote.create(student: student,
+                                                                subject_item: subject_item,
+                                                                value: rand(1..6))
+    end
+  end
+end
